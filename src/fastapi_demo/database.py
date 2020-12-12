6e045acccf3b1e5572ba7b1893665719782aa0a9
Daniel Ruiz <danruicar@gmail.com>
2020-12-12 01:16:17 +0100

    Move alembic .ini files to root folder
    Executing the alembic command gets easier

--- src/fastapi_demo/database.py
+++ src/fastapi_demo/database.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'sqlite:///sqlite.db'
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+DATABASE_URL = f'sqlite:///{BASE_DIR}/sqlite.db'
 
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
